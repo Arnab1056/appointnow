83c788b4b42b63444f63c8b966d04d2bd1806cf0
Arnab <145960793+Arnab1056@users.noreply.github.com>
2025-06-03 16:03:36 +0600

    Update find_doctors.dart
    updated

--- lib/Pages/findDoctors/find_doctors.dart
+++ lib/Pages/findDoctors/find_doctors.dart
@@ -1,15 +1,1 @@
-import 'package:flutter/material.dart';
 
-class FindDoctorScreen extends StatefulWidget {
-  const FindDoctorScreen({super.key});
-
-  @override
-  State<FindDoctorScreen> createState() => _FindDoctorScreenState();
-}
-
-class _FindDoctorScreenState extends State<FindDoctorScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
